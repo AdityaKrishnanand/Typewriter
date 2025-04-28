473928a4c19fea4c86e61b260752ad288551963c
AdityaKrishnanand <adityakrishnanand17@gmail.com>
2025-04-28 20:26:40 +0530

    scoreRoutes addition for retrieving and registering scores

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ dotenv.config();
 
 import express from "express";
 import authRoutes from "./authRoutes.js";
+import scoreRoutes from "./scoreRoutes.js";
 
 const app = express();
 app.use(express.json());
+
 app.use("/api", authRoutes);
+app.use("/api", scoreRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
---
+++ server/scoreRoutes.js
@@ -0,0 +1,50 @@
+import express from "express";
+import prisma from "./prismaClient.js";
+import { authenticateToken } from "./authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/score", authenticateToken, async (req, res) => {
+  const { wpm, accuracy } = req.body;
+
+  const userId = req.user.userId;
+
+  try {
+    const newScore = prisma.score.create({
+      data: {
+        wpm: wpm,
+        accuracy: accuracy,
+        userId: userId,
+      },
+    });
+
+    return res.status(200).json({ message: "Score saved.", score: newScore });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong while saving scores." });
+  }
+});
+
+router.get("/leaderboard", authenticateToken, async (req, res) => {
+  const limit = parseInt(req.query.limit) || 10;
+
+  try {
+    const getScores = prisma.score.findMany({
+      orderBy: { wpm: "desc" },
+      takeLimit: limit,
+      include: { user: { select: { email: true } } },
+    });
+
+    return res.status(200).json({ leaderboard: getScores });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Error while retrieving leaderboard scores.",
+      errorData: error,
+    });
+  }
+});
+
+export default router;
